0316f53adf515595565582fc43ecbf053a9f9980
Alan <alan.mcgillivray@griffith.student.ie>
2025-05-30 15:37:56 +0100

    Added Giant Ancestry to Giant class

--- src/main/java/alan/races/Goliath.java
+++ src/main/java/alan/races/Goliath.java
@@ -6,22 +6,26 @@ import alan.grid_panel.Cell;
 
 public class Goliath extends PlayableCharacter implements RaceInterface{
     final String RACE_NAME = "Goliath";
+    public enum GIANT_ANCESTRY {CLOUD_GIANT, FIRE_GIANT, FROST_GIANT, HILL_GIANT, STONE_GIANT, STORM_GIANT};
+    GIANT_ANCESTRY ancestry;
 
-    public Goliath(String name, String fileName) {
+    public Goliath(String name, String fileName, GIANT_ANCESTRY ancestry) {
         super(name, fileName);
         setCreatureType("Humanoid");
         setSpeed(35);
         setSize(Constants.CREATURE_SIZE.MEDIUM);
         setRaceName(RACE_NAME);
+        this.ancestry = ancestry;
         setSpecialFeatures();
     }
 
-    public Goliath(String name, String fileName, Cell location) {
+    public Goliath(String name, String fileName, Cell location, GIANT_ANCESTRY ancestry) {
         super(name, fileName, location);
         setCreatureType("Humanoid");
         setSpeed(35);
         setSize(Constants.CREATURE_SIZE.MEDIUM);
         setRaceName(RACE_NAME);
+        this.ancestry = ancestry;
         setSpecialFeatures();
     }
 
@@ -30,7 +34,23 @@ public class Goliath extends PlayableCharacter implements RaceInterface{
      */
     @Override
     public final void setSpecialFeatures() {
-        // TODO Auto-generated method stub
+        // Powerful Build
+        grantConditionAdvantage(Constants.CONDITION_KEY.GRAPPLED);
+        switch (ancestry) {
+            case CLOUD_GIANT -> {// 
+            }
+            case FIRE_GIANT -> {// 
+            }
+            case FROST_GIANT -> {// 
+            }
+            case HILL_GIANT -> {// 
+            }
+            case STONE_GIANT -> {// 
+            }
+            case STORM_GIANT -> {// 
+            }
+            default -> throw new AssertionError();
+        }
         
     }
 
