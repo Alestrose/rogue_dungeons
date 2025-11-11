f89c1dd7d9a4935f128d137cecd64f6256582555
Alan <alan.mcgillivray@griffith.student.ie>
2025-11-11 20:36:04 +0000

    Added spells: TashasHideousLaughter

--- src/main/java/alan/spells/level_one/TashasHideousLaughter.java
+++ src/main/java/alan/spells/level_one/TashasHideousLaughter.java
@@ -23,7 +23,7 @@ public class TashasHideousLaughter  extends SpellAbstract implements SpellInterf
 
     @Override
     public void cast(Creature caster, Creature target, Creature[] targetList, Cell cell, DAMAGE_TYPE damage_type, int spellLevel) {
-        if(!rollSpellSaveCheck(target, caster, Constants.ABILITY.WISDOM)) target.ApplyConditionEffect(Constants.CONDITION_KEY.INCAPACITATED, caster);
+        if(!rollSpellSaveCheck(target, caster, getSavingThrow())) target.ApplyConditionEffect(Constants.CONDITION_KEY.INCAPACITATED, caster);
         
     }
 
