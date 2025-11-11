eedf377be465a9859febfa84f9d93eac1056daa6
Alan <alan.mcgillivray@griffith.student.ie>
2025-11-11 19:28:13 +0000

    Added spells: TashasHideousLaughter

---
+++ src/main/java/alan/spells/level_one/TashasHideousLaughter.java
@@ -0,0 +1,46 @@
+package alan.spells.level_one;
+
+import alan.Constants;
+import alan.Constants.DAMAGE_TYPE;
+import alan.creatures.Creature;
+import alan.grid_panel.Cell;
+import alan.spells.SpellAbstract;
+import alan.spells.SpellInterface;
+
+public class TashasHideousLaughter  extends SpellAbstract implements SpellInterface{
+
+    public TashasHideousLaughter(){
+        setSpellName("Tashas Hideous Laughter");
+        setSpellLevel((byte) 1);
+        setSchool(Constants.SCHOOL.ENCHANTMENT);
+        setDamage_type(Constants.DAMAGE_TYPE.FORCE);
+        setSavingThrow(Constants.ABILITY.WISDOM);
+        setConcentration(true);
+        setAction(true);
+        setRange((short) 30);
+        setDuration((byte) 10);
+    }
+
+    @Override
+    public void cast(Creature caster, Creature target, Creature[] targetList, Cell cell, DAMAGE_TYPE damage_type, int spellLevel) {
+        if(!rollSpellSaveCheck(target, caster, Constants.ABILITY.WISDOM)) target.ApplyConditionEffect(Constants.CONDITION_KEY.INCAPACITATED, caster);
+        
+    }
+
+    @Override
+    public String descreiption() {
+        // TODO Auto-generated method stub
+        return """
+               A creature of your choice that you can see within range perceives everything as hilariously funny and falls into fits of laughter if this spell affects it. The target must succeed on a Wisdom saving throw or fall prone, becoming incapacitated and unable to stand up for the duration. A creature with an Intelligence score of 4 or less isn't affected.\r
+               At the end of each of its turns, and each time it takes damage, the target can make another Wisdom saving throw. The target has advantage on the saving throw if it's triggered by damage. On a success, the spell ends.""" //
+        ;
+    }
+
+    @Override
+    public void onLevelUp(int lvl) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    
+}
